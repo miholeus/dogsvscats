00d096df2547148c8bd7345e1075133048638362
miholeus <me@miholeus.com>
2021-05-30 17:07:17 +0300

    updated template

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from typing import Optional
+import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory=os.path.join(dir_path, "static")), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+templates = Jinja2Templates(directory=os.path.join(dir_path, "templates"))
 
 
-@app.get("/items/{item_id}")
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/api/predict/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
